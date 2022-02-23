380afc7de80e9a97675a27593994a924b414eb9d
giangth10 <giangth10.sqtt@gmail.com>
2022-02-23 15:55:25 +0700

    Add files via upload

---
+++ MetazToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract MetazToken is ERC20, Ownable {
+    constructor() ERC20("MetazToken", "METAZ") {}
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
